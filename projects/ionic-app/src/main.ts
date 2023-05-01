12396a10e99b602c331229e5072a802cdca013ef
Riccardo Rossetti <riccardo.rossetti@tgl.co>
2023-05-01 11:26:35 +1000

    PWA app configured

--- projects/ionic-app/src/main.ts
+++ projects/ionic-app/src/main.ts
@@ -1,4 +1,4 @@
-import { enableProdMode, importProvidersFrom } from '@angular/core';
+import { enableProdMode, importProvidersFrom, isDevMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
@@ -10,6 +10,7 @@ import {
   DARK_MODE,
   DARK_MODE_DEFAULT,
 } from './app/shared/utils/injection-tokens';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 if (environment.production) {
   enableProdMode();
@@ -21,7 +22,6 @@ bootstrapApplication(AppSelectorComponent, {
     { provide: DARK_MODE, useValue: DARK_MODE_DEFAULT },
     importProvidersFrom(
       IonicModule.forRoot({
-
         platform: {
           /** The default `desktop` function returns false for devices with a touchscreen.
            * This is not always wanted, so this function tests the User Agent instead.
@@ -34,6 +34,12 @@ bootstrapApplication(AppSelectorComponent, {
             return !isMobile;
           },
         },
+      }),
+      ServiceWorkerModule.register('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        // Register the ServiceWorker as soon as the application is stable
+        // or after 30 seconds (whichever comes first).
+        registrationStrategy: 'registerWhenStable:30000',
       })
     ),
     provideRouter(ROUTES),
