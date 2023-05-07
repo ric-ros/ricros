e6e731ef138a723a9b63ed167b4a297f655b3d26
Riccardo Rossetti <riccardo.rossetti@tgl.co>
2023-05-07 18:43:14 +1000

    Dynamic date

--- projects/ionic-app/src/app/shared/content/CV/cv.component.ts
+++ projects/ionic-app/src/app/shared/content/CV/cv.component.ts
@@ -11,4 +11,31 @@ import { IonicModule } from '@ionic/angular';
 })
 export class CvComponent {
   constructor() {}
+
+  calculateTillNow(date: string): string {
+    // Calculate the number of months between the specific date and the current date
+    const monthsDiff =
+      (new Date().getTime() - new Date(date).getTime()) /
+      1000 /
+      60 /
+      60 /
+      24 /
+      30.44;
+
+    // Format the result as a string
+    let result;
+    if (monthsDiff < 12) {
+      result = `${Math.floor(monthsDiff)} month${
+        Math.floor(monthsDiff) === 1 ? '' : 's'
+      }`;
+    } else {
+      const years = Math.floor(monthsDiff / 12);
+      const months = Math.floor(monthsDiff % 12);
+      result = `${years} year${years === 1 ? '' : 's'} and ${months} month${
+        months === 1 ? '' : 's'
+      }`;
+    }
+
+    return result;
+  }
 }
